6b8e795343d6fc1756d2af11dceaaf6a4725c24a
sdarron <zaytcev@bk.ru>
2023-03-08 08:40:18 +0300

    3

---
+++ src/Pell.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Pell {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите число от 0 до 30 включительно");
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            long pellNumber = 0L;
+            long pellNumber0 = 0L;
+            long pellNumber1 = 1L;
+            long pellNumber2 = 0L;
+
+            if (n >= 0 && n <= 30) {
+                if (n == 1) {
+                    pellNumber = 1;
+                } else if (n > 1 && n <= 30){
+                    for (int i = 2; i <= n; i++) {
+                        pellNumber2 = 2 * pellNumber1 + pellNumber0;
+                        pellNumber0 = pellNumber1;
+                        pellNumber1 = pellNumber2;
+                    }
+                    pellNumber = pellNumber2;
+                }
+                System.out.println("Число Пелля для " + n + " равно " + pellNumber);
+            } else
+                System.out.println("Число не соответствует условию.");
+        }
+    }
+}
+
